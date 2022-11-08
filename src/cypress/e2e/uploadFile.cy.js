09be0a0850ea136c8b16ef845dc7588f338d1b8d
Аня Философ <afilosof@justclick.ru>
2022-11-08 12:47:59 +0300

    file upload test

--- src/cypress/e2e/uploadFile.cy.js
+++ src/cypress/e2e/uploadFile.cy.js
@@ -3,6 +3,7 @@ describe('Test upload package', () => {
     cy.visit('https://www.browserling.com/tools/image-to-base64');
     cy.get('[id="file-select"]').attachFile({ filePath: 'instaa.jpeg' });
     cy.get('[id="submit"]').click();
+    cy.task('log', 'Hello World!');
     cy.get('[id="image-to-base64-text"]').should('be.visible');
   });
 });
